1b69656bf62ef2dd71ac2eb02089ecb2304a53a7
xeriscape <fyb@refulgenceT430>
2016-07-24 17:32:59 +0200

    Initial character/effect work

--- Leafletrop/src/game/mechanics/character/Effect.java
+++ Leafletrop/src/game/mechanics/character/Effect.java
@@ -1,9 +1,12 @@
 package game.mechanics.character;
 
 import misc.Identity;
+import util2d.core.GLOBALS;
 
 public class Effect extends Identity {
 	public AbilityScores statDelta;
+	public int timeToLive;
+	public boolean usesTimeToLive;
 	private Identity source;
 	
 	/**
@@ -34,9 +37,19 @@ public class Effect extends Identity {
 	 * @param friendlyName What to call the Effect
 	 */
 	public Effect(Identity source, AbilityScores statDelta, String friendlyName) {
+		this(source, statDelta, friendlyName, 0);
+	}
+	
+	public Effect(Identity source, AbilityScores statDelta, String friendlyName, double secondsToLive) {
+		this(source, statDelta, friendlyName, GLOBALS.secondsToFrames(secondsToLive));
+	}
+	
+	public Effect(Identity source, AbilityScores statDelta, String friendlyName, int framesToLive) {
 		super(friendlyName); //Identity: New ID, blank name
 		this.source = source;
 		this.statDelta = statDelta;
+		this.timeToLive = framesToLive;
+		this.usesTimeToLive = (this.timeToLive != 0);
 	}
 
 	public Identity getSource() {
